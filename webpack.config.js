833bef44c4b8f81c6aaba6af8e8dffddfa93c11e
Fahd Rahali <rahali.fahd@gmail.com>
2022-03-22 14:03:57 +0100

    setup a local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
+  devServer : {
+    static: './dist'
+  },
   module: {
     rules: [
       {
