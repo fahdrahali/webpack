0c239e8e646fad2568c47dfb17567af3f075d665
Fahd Rahali <rahali.fahd@gmail.com>
2022-03-22 13:55:25 +0100

    add a sass style file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-
-plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
-     template: path.resolve(__dirname, "src", "index.html")
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
-};
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+}
